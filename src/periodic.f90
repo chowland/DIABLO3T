78d20689b0735d7ee5fd7178d702a6554e82f514
chowland <c.j.howland@utwente.nl>
2022-06-21 13:59:14 +0100

    specify scalar-dependent time-step parameter

--- src/periodic.f90
+++ src/periodic.f90
@@ -17,12 +17,13 @@ subroutine rk_per_1(rk_step)
     real(dp) :: temp1, temp2, temp3, temp4, temp6
 
     temp4 = h_bar(rk_step)*delta_t
-    temp1 = kappa(1)*temp4*0.5_dp
+    temp1 = nu*temp4*0.5_dp
     temp2 = beta_bar(rk_step)*temp4
     temp3 = zeta_bar(rk_step)*temp4
 
     ! Explicit part of diffusive term
     do n=1,n_th
+        temp1 = 0.5_dp*kappa(n)*temp4
         do k=cstart(3),cend(3)
             do j=cstart(2),cend(2)
                 do i=cstart(1),cend(1)
@@ -46,35 +47,30 @@ subroutine rk_per_1(rk_step)
     end if
     cfth(:,:,:,:) = 0.0_dp
 
-    ! Transform the scalar concentration to physical space
-    do n=1,n_th
-        call decomp_2d_fft_3d(cth(:,:,:,n), th(:,:,:,n))
-    end do
-    th = th/rnx/rny/rnz
-
     !! ADVECT THE TRACER - NONLINEAR TERMS
-    do n=1,n_th
-        do k=xstart(3),xend(3)
-            do j=xstart(2),xend(2)
-                do i=xstart(1),xend(1)
-                    sth(i,j,k) = sin(2*pi/Ly*gy(j))*th(i,j,k,n)
-                end do
-            end do
-        end do
-        call decomp_2d_fft_3d(sth, csth)
-        do k=cstart(3),cend(3)
-            do j=cstart(2),cend(2)
-                do i=cstart(1),cend(1)
-                    cfth(i,j,k,n) = cfth(i,j,k,n) - cikx(i)*csth(i,j,k)
-                    crth(i,j,k,n) = crth(i,j,k,n) + temp2*cfth(i,j,k,n)
-                end do
-            end do
-        end do
+    ! do n=1,n_th
+    !     do k=xstart(3),xend(3)
+    !         do j=xstart(2),xend(2)
+    !             do i=xstart(1),xend(1)
+    !                 sth(i,j,k) = sin(2*pi/Ly*gy(j))*th(i,j,k,n)
+    !             end do
+    !         end do
+    !     end do
+    !     call decomp_2d_fft_3d(sth, csth)
+    !     do k=cstart(3),cend(3)
+    !         do j=cstart(2),cend(2)
+    !             do i=cstart(1),cend(1)
+    !                 cfth(i,j,k,n) = cfth(i,j,k,n) - cikx(i)*csth(i,j,k)
+    !                 crth(i,j,k,n) = crth(i,j,k,n) + temp2*cfth(i,j,k,n)
+    !             end do
+    !         end do
+    !     end do
         
-    end do
+    ! end do
 
     ! Solve the implicit system for the intermediate field
     do n=1,n_th
+        temp1 = 0.5_dp*kappa(n)*temp4
         do k=cstart(3),cend(3)
             do j=cstart(2),cend(2)
                 do i=cstart(1),cend(1)
@@ -87,6 +83,11 @@ subroutine rk_per_1(rk_step)
 
     ! Compute second fractional step...
 
+    ! Update the scalar fields in physical space at the end of the time step
+    do n=1,n_th
+        call decomp_2d_fft_3d(cth(:,:,:,n), th(:,:,:,n))
+    end do
+    th = th/rnx/rny/rnz
 
 end subroutine rk_per_1
     
