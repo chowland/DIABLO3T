7dd744b57c85e7d8a13270c9d463a73d7738a26a
chowland <c.j.howland@utwente.nl>
2022-06-24 09:15:04 +0000

    add working Taylor-Green vortex

--- src/user_IC.f90
+++ src/user_IC.f90
@@ -1,11 +1,11 @@
 module InitialConditions
     use param
     use grid
-    use decomp_2d, only: xstart, xend
+    use decomp_2d, only: xstart, xend, nrank
     use decomp_2d_fft
 
     private
-    public :: SetTemperatureIC, SetVelocityIC
+    public :: SetTemperatureIC, SetVelocityIC, SetPressure
 contains
 
 subroutine SetTemperatureIC
@@ -40,16 +40,23 @@ subroutine SetVelocityIC
     do k=xstart(3),xend(3)
         do j=xstart(2),xend(2)
             do i=xstart(1),xend(1)
-                u(i,j,k,1) = sin(gy(j))
+                u(i,j,k,1) = cos(2*gx(i)) * sin(2*gy(j))
+                u(i,j,k,2) =-sin(2*gx(i)) * cos(2*gy(j))
             end do
         end do
     end do
 
-    u(:,:,:,2:) = 0.0_dp
+    u(:,:,:,3) = 0.0_dp
 
     do n=1,3
         call decomp_2d_fft_3d(u(:,:,:,n), cu(:,:,:,n))
     end do
+
+    if (nrank==0) write(*,*) 'cu(1,:,1,1): ', cu(1,:,1,1)
+    if (nrank==0) write(*,*) 'cu(2,:,1,1): ', cu(2,:,1,1)
+
+    if (nrank==0) write(*,*) 'cu(1,:,1,2): ', cu(1,:,1,2)
+    if (nrank==0) write(*,*) 'cu(2,:,1,2): ', cu(2,:,1,2)
 
 end subroutine SetVelocityIC
 
